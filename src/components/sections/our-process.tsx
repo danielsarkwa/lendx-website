483412dd7c706251d321797964c86b9fbe3cfbea
danielsarkwa <dhannysark@outlook.com>
2025-06-07 02:21:49 +0300

    refactor: improve layout and styling of OurProcess component and steps

--- src/components/sections/our-process.tsx
+++ src/components/sections/our-process.tsx
@@ -3,23 +3,36 @@ import Image from 'next/image';
 
 export default function OurProcess() {
   return (
-    <section id='our-process' className='bg-[#CEF8F4]'>
+    <section
+      id='our-process'
+      className='bg-[#CEF8F4] rounded-[50px] py-[90px] px-[100px] flex flex-col items-center gap-[50px]'
+    >
       {/* section header */}
-      <div>
-        <h2>How it works</h2>
-        <p>
+      <div className='flex flex-col items-center w-fit'>
+        <h2 className='text-5xl font-semibold leading-tight text-center'>
+          How it works
+        </h2>
+        <p className='text-xl text-[#647581] font-regular leading-tight max-w-[470px] text-center'>
           Get your loan approved and deposited in just 3 days — fast, simple,
           and secure.
         </p>
       </div>
 
       {/* steps container */}
-      <div>
+      <div className='flex gap-[30px]'>
         {steps.map((step, index) => (
-          <div key={index}>
-            <div>
-              <h5>{step.title}</h5>
-              <p>{step.description}</p>
+          <div
+            key={index}
+            className='p-5 rounded-[10px] flex flex-col justify-between items-center gap-9 max-w-[404px]'
+            style={{ backgroundColor: step.color }}
+          >
+            <div className='flex flex-col gap-1'>
+              <h5 className='text-xl font-semibold leading-tight'>
+                {step.title}
+              </h5>
+              <p className='text-lg text-[#51626D] font-regular leading-tight'>
+                {step.description}
+              </p>
             </div>
             <div>
               <Image
--- src/constants/steps.ts
+++ src/constants/steps.ts
@@ -4,16 +4,19 @@ export const steps = [
     description:
       'Submit your loan application online in just a few quick steps.',
     image: '/illustrations/send.svg',
+    color: '#8BEEE5',
   },
   {
     title: 'Application processing',
     description:
       'Our team reviews your application promptly to ensure quick approval.',
     image: '/illustrations/security.svg',
+    color: '#8BEAEE',
   },
   {
     title: 'Loan deposit',
     description: 'Funds are deposited directly into your account within days.',
     image: '/illustrations/receive-cash.svg',
+    color: '#8BEECD',
   },
 ];
