a4fca38c4a8b1c67daf639a5804ff1e354792dfe
danielsarkwa <dhannysark@outlook.com>
2025-06-06 16:24:31 +0300

    Add partner logos and custom fonts

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        switzer: ['var(--font-switzer)', 'Inter', 'sans-serif'],
+      },
+    },
+  },
+};
