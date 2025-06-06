ec73cba38e15999cfc545c47c8f78e8888879c2d
danielsarkwa <dhannysark@outlook.com>
2025-06-06 22:28:26 +0300

    added styling for hero lhs

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -3,25 +3,38 @@ import LoadForm from '@/components/loan-form';
 
 export default function Hero() {
   return (
-    <section className=''>
+    <section className='flex'>
       {/* lhs */}
-      <div>
-        <h1>Fast Cash for Your Bold Lifes</h1>
-        <p>
-          Lendx delivers instant loans with zero hassle. Our user-friendly
-          platform let’s you access cash in minutes from anywhere. Power your
-          bold lifestyle with financial freedom—apply now and seize the moment!
-        </p>
-        <Image
-          src='/graphics/pen-scribble-1.svg'
-          alt='Pen Scribble'
-          width={271}
-          height={19}
-        />
+      <div className='py-[270px] flex-1'>
+        <div
+          className='flex flex-col max-w-[556px] gap-6 mx-auto'
+          style={{ position: 'relative' }}
+        >
+          <h1 className='text-[60px] font-semibold leading-tight'>
+            Fast Cash for Your Bold Life
+          </h1>
+          <p className='text-xl text-[#647581] font-regular leading-tight'>
+            Lendx delivers instant loans with zero hassle. Our user-friendly
+            platform let’s you access cash in minutes from anywhere. Power your
+            bold lifestyle with financial freedom—apply now and seize the
+            moment!
+          </p>
+          <Image
+            src='/graphics/pen-scribble-1.svg'
+            alt='Pen Scribble'
+            width={271}
+            height={19}
+            style={{
+              position: 'absolute',
+              top: '60px',
+              left: '0px',
+            }}
+          />
+        </div>
       </div>
 
       {/* rhs */}
-      <div>
+      <div className='flex-1'>
         <LoadForm />
       </div>
     </section>
